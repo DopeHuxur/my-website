c961cf2174ac8404952154fb188f4c6cf34b4530
Anwar K. Sadat <anwarksadat@outlook.com>
2025-03-04 09:50:48 +0600

    added slideshow and other adjustments

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        document.getElementById('slideshow').style.display = 'none';
+        document.getElementById('home-screen').classList.add('active');
+    }, 3000); // 2 second display + 1 second animation
+});
