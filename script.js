f7d22f19b0d6fa07b0087b4cb35dc65bb7f5a7e5
Anwar K. Sadat <anwarksadat@outlook.com>
2025-03-04 11:40:59 +0600

    added dark mode

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// HAMBORGIR MENU
 function toggleMenu() {
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburger-icon');
@@ -5,9 +6,23 @@ function toggleMenu() {
     icon.classList.toggle('open');
 }
 
+// SLIDESHOW QUOTE
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         document.getElementById('slideshow').style.display = 'none';
         document.getElementById('home-screen').classList.add('active');
     }, 3000); // 2 second display + 1 second animation
+});
+
+// DARK MODE
+const themeToggleButtons = document.querySelectorAll('#theme-toggle');
+themeToggleButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+        if (this.textContent === "🌙") {
+            this.textContent = "☀️";
+        } else {
+            this.textContent = "🌙";
+        }
+    });
 });
